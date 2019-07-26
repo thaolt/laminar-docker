9ff19ebf8009dd347dd6ca9f7e293a708c128331
Thao le <thaolt@songphi.com>
2019-07-27 04:23:21 +0700

    chmod heroku start, replace port with env{PORT} on boot

--- heroku_start.sh
+++ heroku_start.sh
@@ -7,8 +7,7 @@ cp /authorized_keys ~/.ssh/authorized_keys
 service cron start
 service ssh start
 
+sed -i "s/\#LAMINAR_BIND_HTTP=\*\:8080/LAMINAR_BIND_HTTP=*:${PORT}/g" /etc/laminar.conf
+/usr/bin/laminard &
+
 ssh -R 0:localhost:22 serveo.net
-
-/usr/bin/laminard
-
-
